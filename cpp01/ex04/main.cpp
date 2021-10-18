93112eac0fca47f30814f3fee5d95bccee5d463c
harinez2 <yonishi@student.42tokyo.jp>
2021-10-18 21:56:21 +0900

    added write to file func

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 
-static int	read_file(std::string filename, std::string &read_data)
+static int	read_from_file(std::string filename, std::string &read_data)
 {
 	std::ifstream	ifs(filename);
 	if (!ifs)
@@ -30,11 +30,18 @@ static void	replace_str(
 	std::string::size_type pos = read_data.find(replacing_str);
 	while (pos != std::string::npos)
 	{
-		std::cout << "found at:" << pos << std::endl;
 		read_data.erase(pos, replacing_str.length());
 		read_data.insert(pos, replaced_str);
 		pos = read_data.find(replacing_str, pos + replaced_str.length());
 	}
+}
+
+static int	write_to_file(std::string filename, std::string &read_data)
+{
+	filename += ".replace";
+
+	std::cout << read_data << std::endl;
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -46,9 +53,10 @@ int	main(int argc, char **argv)
 	}
 
 	std::string	read_data = "";
-	if (read_file(argv[1], read_data) != 0)
+	if (read_from_file(argv[1], read_data) != 0)
 		return (-1);
 	replace_str(read_data, argv[2], argv[3]);
-	std::cout << read_data << std::endl;
+	if (write_to_file(argv[1], read_data) != 0)
+		return (-1);
 	return (0);
 }
