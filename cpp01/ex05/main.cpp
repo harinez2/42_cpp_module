b1232423258a92862aa4f1ca6597eafd017bd5e3
harinez2 <yonishi@student.42tokyo.jp>
2021-10-22 23:39:35 +0900

    formatted output of ex05

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -3,9 +3,16 @@
 int	main(void)
 {
 	Karen k;
+
 	k.complain("DEBUG");
+	std::cout << std::endl;
+
 	k.complain("INFO");
+	std::cout << std::endl;
+	
 	k.complain("WARNING");
+	std::cout << std::endl;
+	
 	k.complain("ERROR");
 	return (0);
 }
