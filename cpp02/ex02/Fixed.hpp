6df163e5d39d654a5fe8a12e31beefcfd5a78662
harinez2 <yonishi@student.42tokyo.jp>
2021-10-28 01:00:42 +0900

    initial of ex02

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int					_fixed_point_value;
+	static const int	_fractional_bits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed &obj);
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed&				operator=(const Fixed& obj);
+	~Fixed();
+	int					getRawBits(void) const;
+	void				setRawBits(int const raw);
+	float				toFloat(void) const;
+	int					toInt(void) const;
+};
+
+std::ostream&	operator<<(std::ostream& os, const Fixed& obj);
+
+#endif
