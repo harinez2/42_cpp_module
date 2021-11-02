f6b262aa8a93f98db9ba5e968524888291361333
harinez2 <yonishi@student.42tokyo.jp>
2021-11-03 02:21:48 +0900

    base code <intermediate>

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -1,6 +1,7 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap()
+ClapTrap::ClapTrap(std::string name)
+	: _name(name), _hit_points(0), _energy_points(0), _attack_damage(0)
 {
 }
 
@@ -11,9 +12,49 @@ ClapTrap::ClapTrap(const ClapTrap &obj)
 
 ClapTrap&	ClapTrap::operator=(const ClapTrap& obj)
 {
+	_name = obj.getName();
+	_hit_points = obj.getHitPoints();
+	_energy_points = obj.getEnergyPoints();
+	_attack_damage = obj.getAttackPoints();
 	return (*this);
 }
 
 ClapTrap::~ClapTrap()
 {
+}
+
+std::string	ClapTrap::getName(void) const
+{
+	return _name;
+}
+
+int	ClapTrap::getHitPoints(void) const
+{
+	return _hit_points;
+}
+
+int	ClapTrap::getEnergyPoints(void) const
+{
+	return _energy_points;
+}
+
+int	ClapTrap::getAttackPoints(void) const
+{
+	return _attack_damage;
+}
+
+void	ClapTrap::attack(std::string const & target)
+{
+	std::cout << "ClapTrap " << _name << " attacks " << target;
+	std::cout << ", causing " << _attack_damage << " points of damage!" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "takeDamage " << amount << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	std::cout << "beRepaired" << amount << std::endl;
 }
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -2,15 +2,29 @@
 #define CLAPTRAP_HPP
 
 #include <iostream>
+#include <string>
 
 class ClapTrap
 {
 private:
+	std::string			_name;
+	int					_hit_points;
+	int					_energy_points;
+	int					_attack_damage;
 public:
-	ClapTrap();
+	ClapTrap(std::string name);
 	ClapTrap(const ClapTrap &obj);
 	ClapTrap&			operator=(const ClapTrap& obj);
 	~ClapTrap();
+
+	std::string		getName(void) const;
+	int					getHitPoints(void) const;
+	int					getEnergyPoints(void) const;
+	int					getAttackPoints(void) const;
+
+	void				attack(std::string const & target);
+	void				takeDamage(unsigned int amount);
+	void				beRepaired(unsigned int amount);
 };
 
 #endif
