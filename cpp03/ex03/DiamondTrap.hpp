231f6d147c2f10914a762d6c73e1e55b90e2a882
harinez2 <yonishi@student.42tokyo.jp>
2021-11-05 21:47:30 +0900

    added new file

---
+++ cpp03/ex03/DiamondTrap.hpp
@@ -0,0 +1,15 @@
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONDTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class DiamondTrap : public ClapTrap
+{
+private:
+public:
+	DiamondTrap(std::string name);
+	~DiamondTrap(void);
+
+};
+
+#endif
