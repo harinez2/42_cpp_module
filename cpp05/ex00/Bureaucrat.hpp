2c3e616a7a2c75fc2a1745b8513b91f0a6117876
harinez2 <yonishi@student.42tokyo.jp>
2021-11-10 21:07:39 +0900

    initial commit

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,13 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+class Bureaucrat
+{
+public:
+	Bureaucrat();
+	Bureaucrat(const Bureaucrat &);
+	Bureaucrat & operator=(const Buraucrat &);
+	~Bureaucrat();
+};
+
+#endif
