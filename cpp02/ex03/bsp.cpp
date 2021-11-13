71638df31cbd4a9fa82f75a41d0ed0bb0a10f0b4
harinez2 <yonishi@student.42tokyo.jp>
2021-11-13 18:17:08 +0900

    fixed edge case

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -11,11 +11,13 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
   Fixed ac_y = c.getY() - a.getY();
   Fixed bc_x = c.getX() - b.getX();
   Fixed bc_y = c.getY() - b.getY();
-  bool s_ab = ab_x * ap_y - ab_y * ap_x > 0;
+  bool sign_ab = ab_x * ap_y - ab_y * ap_x >= 0;
+  bool sign_ac = ac_x * ap_y - ac_y * ap_x >= 0;
+  bool sign_bc = bc_x * bp_y - bc_y * bp_x >= 0;
 
-  if(ac_x * ap_y - ac_y * ap_x > 0 == s_ab)
+  if(sign_ac == sign_ab)
     return false;
-  if(bc_x * bp_y - bc_y * bp_x > 0 != s_ab)
+  if(sign_bc != sign_ab)
     return false;
   return true;
 }
