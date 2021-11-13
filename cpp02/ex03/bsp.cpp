23b61ff9ff119cb6b24243e52016d9ec80024c62
harinez2 <yonishi@student.42tokyo.jp>
2021-11-13 20:59:37 +0900

    fixed for edge case

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -1,24 +1,49 @@
 #include "Point.hpp"
 #include "Fixed.hpp"
 
+#include <cmath>
+
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
   Fixed ap_x = point.getX() - a.getX();
   Fixed ap_y = point.getY() - a.getY();
-  Fixed bp_x = point.getX() - b.getX();
-  Fixed bp_y = point.getY() - b.getY();
   Fixed ab_x = b.getX() - a.getX();
   Fixed ab_y = b.getY() - a.getY();
   Fixed ac_x = c.getX() - a.getX();
   Fixed ac_y = c.getY() - a.getY();
+
+  Fixed prod_ab = ab_x * ap_y - ab_y * ap_x;
+  if (roundf(prod_ab.toFloat()) == 0)
+    return false;
+  bool sign_ab = roundf(prod_ab.toFloat()) >= 0;
+
+  Fixed prod_ac = ac_x * ap_y - ac_y * ap_x;
+  if (roundf(prod_ac.toFloat()) == 0)
+    return false;
+  bool sign_ac = roundf(prod_ac.toFloat()) >= 0;
+
+  if(sign_ab == sign_ac)
+    return false;
+
+
+  Fixed bp_x = point.getX() - b.getX();
+  Fixed bp_y = point.getY() - b.getY();
+  Fixed ba_x = a.getX() - b.getX();
+  Fixed ba_y = a.getY() - b.getY();
   Fixed bc_x = c.getX() - b.getX();
   Fixed bc_y = c.getY() - b.getY();
-  bool sign_ab = ab_x * ap_y - ab_y * ap_x >= 0;
-  bool sign_ac = ac_x * ap_y - ac_y * ap_x >= 0;
-  bool sign_bc = bc_x * bp_y - bc_y * bp_x >= 0;
 
-  if(sign_ac == sign_ab)
+  Fixed prod_ba = ba_x * bp_y - ba_y * bp_x;
+  if (roundf(prod_ba.toFloat()) == 0)
     return false;
-  if(sign_bc != sign_ab)
+  bool sign_ba = roundf(prod_ba.toFloat()) >= 0;
+
+  Fixed prod_bc = bc_x * bp_y - bc_y * bp_x;
+  if (roundf(prod_bc.toFloat()) == 0)
     return false;
+  bool sign_bc = roundf(prod_bc.toFloat()) >= 0;
+  
+  if(sign_ba == sign_bc)
+    return false;
+
   return true;
 }
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -7,29 +7,31 @@
 bool bsp(Point const a, Point const b, Point const c, Point const point);
 
 void test_bsp(Point const a, Point const b, Point const c, Point const point) {
-  std::cout << "point    ";
-  std::cout << std::setw(6) << point.getX() << " , ";
-  std::cout << std::setw(6) << point.getY() << " , ret:";
-  std::cout << bsp(a, b, c, point) << std::endl;
+  std::cout << "point    "
+      << std::setw(6) << point.getX() << " , "
+      << std::setw(6) << point.getY() << " , ret:"
+      << bsp(a, b, c, point) << std::endl;
 }
 
 int main(void) {
   std::cout << "[triangle]" << std::endl;
   Point a(0, 0);
-  Point b(3.0, 0);
-  Point c(1.5, 1.5);
+  Point b(30.0, 0);
+  Point c(15.0, 15.0);
   std::cout << "a:" << std::setw(6) << a.getX() << "," << std::setw(6) << a.getY() << std::endl;
   std::cout << "b:" << std::setw(6) << b.getX() << "," << std::setw(6) << b.getY() << std::endl;
   std::cout << "c:" << std::setw(6) << c.getX() << "," << std::setw(6) << c.getY() << std::endl;
 
   std::cout << "[judgement]" << std::endl;
-  Point inside(1.5, 0.5);
-  Point outside(3.0, 3.0);
-  Point vertex(1.5, 1.5);
-  Point on_the_edge(0.5, 0.5);
+  Point inside(15.0, 5.0);
+  Point outside(30.0, 15.0);
+  Point vertex(15.0, 15.0);
+  Point on_the_edge1(5.0, 5.0);
+  Point on_the_edge2(25.0, 5.0);
   test_bsp(a, b, c, inside);
   test_bsp(a, b, c, outside);
   test_bsp(a, b, c, vertex);
-  test_bsp(a, b, c, on_the_edge);
+  test_bsp(a, b, c, on_the_edge1);
+  test_bsp(a, b, c, on_the_edge2);
   return 0;
 }
