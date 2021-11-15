8e8e461653bd4ca81097c057055b7d4e04b5572f
harinez2 <yonishi@student.42tokyo.jp>
2021-11-16 00:38:50 +0900

    updated main

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -2,58 +2,33 @@
 
 #include <iostream>
 
+void test_bureaaucrat(const std::string& name, int initial_grade) {
+  try {
+    Bureaucrat b(name, initial_grade);
+    std::cout << b << std::endl;
+    b.incrementGrade();
+    std::cout << b << std::endl;
+    b.decrementGrade();
+    std::cout << b << std::endl;
+    b.decrementGrade();
+    std::cout << b << std::endl;
+  }
+  catch (std::exception & e) {
+    std::cerr << e.what() << std::endl;
+  }
+  std::cout << std::endl;
+}
+
 int main(void)
 {
   // normal case
-  try {
-    Bureaucrat b("me", 10);
-    std::cout << b << std::endl;
-    b.incrementGrade();
-    std::cout << b << std::endl;
-    b.decrementGrade();
-    std::cout << b << std::endl;
-  }
-  catch (std::exception & e) {
-    std::cerr << e.what() << std::endl;
-  }
-  std::cout << std::endl;
+  test_bureaaucrat("me", 10);
 
   // error cases
-
-  try {
-    Bureaucrat b("me", 0);
-  }
-  catch (std::exception & e) {
-    std::cerr << e.what() << std::endl;
-  }
-  std::cout << std::endl;
-
-  try {
-    Bureaucrat b("me", 151);
-  }
-  catch (std::exception & e) {
-    std::cerr << e.what() << std::endl;
-  }
-  std::cout << std::endl;
-
-  try {
-    Bureaucrat b("me", 1);
-    std::cout << b << std::endl;
-    b.incrementGrade();
-  }
-  catch (std::exception & e) {
-    std::cerr << e.what() << std::endl;
-  }
-  std::cout << std::endl;
-
-  try {
-    Bureaucrat b("me", 150);
-    std::cout << b << std::endl;
-    b.decrementGrade();
-  }
-  catch (std::exception & e) {
-    std::cerr << e.what() << std::endl;
-  }
+  test_bureaaucrat("me", 0);
+  test_bureaaucrat("me", 151);
+  test_bureaaucrat("me", 1);
+  test_bureaaucrat("me", 150);
 
   return 0;
 }
