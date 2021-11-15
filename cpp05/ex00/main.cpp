cc70dbe8397a1329e6b48202b42be4ddebb47545
harinez2 <yonishi@student.42tokyo.jp>
2021-11-16 00:33:24 +0900

    updated main

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -1,13 +1,32 @@
 #include "Bureaucrat.hpp"
 
+#include <iostream>
+
 int main(void)
 {
+  // normal case
+  try {
+    Bureaucrat b("me", 10);
+    std::cout << b << std::endl;
+    b.incrementGrade();
+    std::cout << b << std::endl;
+    b.decrementGrade();
+    std::cout << b << std::endl;
+  }
+  catch (std::exception & e) {
+    std::cerr << e.what() << std::endl;
+  }
+  std::cout << std::endl;
+
+  // error cases
+
   try {
     Bureaucrat b("me", 0);
   }
   catch (std::exception & e) {
     std::cerr << e.what() << std::endl;
   }
+  std::cout << std::endl;
 
   try {
     Bureaucrat b("me", 151);
@@ -15,17 +34,21 @@ int main(void)
   catch (std::exception & e) {
     std::cerr << e.what() << std::endl;
   }
+  std::cout << std::endl;
 
   try {
     Bureaucrat b("me", 1);
+    std::cout << b << std::endl;
     b.incrementGrade();
   }
   catch (std::exception & e) {
     std::cerr << e.what() << std::endl;
   }
+  std::cout << std::endl;
 
   try {
     Bureaucrat b("me", 150);
+    std::cout << b << std::endl;
     b.decrementGrade();
   }
   catch (std::exception & e) {
