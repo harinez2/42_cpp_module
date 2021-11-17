2bad8fd6713c60bbd2960d37fde138d79ed7f1cd
harinez2 <yonishi@student.42tokyo.jp>
2021-11-17 19:44:38 +0900

    brush up brain

--- cpp04/ex01/Brain.hpp
+++ cpp04/ex01/Brain.hpp
@@ -3,17 +3,16 @@
 
 #include <string>
 
-class Brain
-{
-protected:
-	std::string		ideas[100];
-public:
-	Brain(void);
-	Brain(const Brain& obj);
-	Brain(const Brain* obj);
-	Brain&			operator=(const Brain& obj);
-	virtual ~Brain(void);
+class Brain {
+ public:
+  Brain();
+  Brain(const Brain& obj);
+  Brain(const Brain* obj);
+  Brain& operator=(const Brain& obj);
+  virtual ~Brain();
 
+ protected:
+  std::string ideas[100];
 };
 
 #endif
