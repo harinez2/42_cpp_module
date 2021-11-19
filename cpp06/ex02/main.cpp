0724c97b76b77d30d7b740cf3cb8fc83e0e6790b
harinez2 <yonishi@student.42tokyo.jp>
2021-11-20 02:43:09 +0900

    ex02 added some intermediate code

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -3,25 +3,44 @@
 #include "B.hpp"
 #include "C.hpp"
 
-Base * generate(void) {
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+Base* generate(void) {
+  if (std::rand() % 3 == 0) {
+    std::cout << "generated A instance." << std::endl;
+    return new A();
+  }
+  else if (std::rand() % 3 == 1) {
+    std::cout << "generated B instance." << std::endl;
+    return new B;
+  }
+  else {
+    std::cout << "generated C instance." << std::endl;
+    return new C;
+  }
+  return NULL;
 }
 
-void identify(Base* p) {
-
+void identify(const Base* p) {
+  (void)p;
 }
 
-void identify(Base& p) {
-
+void identify(const Base& p) {
+  (void)p;
 }
 
 void test_generate_and_identify() {
   Base* b = generate();
   identify(b);
   identify(*b);
+  delete b;
+  std::cout << std::endl;
 }
 
 int main(void) {
+  std::srand(static_cast<unsigned int>(time(NULL)));
   for (int i = 0; i < 5; ++i)
     test_generate_and_identify();
   return 0;
