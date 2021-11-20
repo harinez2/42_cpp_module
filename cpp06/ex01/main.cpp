4ea45153d45ef5018fc793d21fcff5afa0990b72
harinez2 <yonishi@student.42tokyo.jp>
2021-11-21 00:33:28 +0900

    added test code

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
+#include <stdint.h>
 
 struct Data {
-
+  int i;
+  char c;
 };
 
-unitptr_t serialize(Data* ptr) {
-
+uintptr_t serialize(Data* ptr) {
+  uintptr_t ip = reinterpret_cast<uintptr_t>(ptr);
+  std::cout << "serialized uintptr_t : " << std::hex << ip << std::endl;
+  std::cout << std::endl; 
+  return ip;
 }
 
 Data* deserialize(uintptr_t raw) {
-
+  return reinterpret_cast<Data*>(raw);
 }
 
-int main(int argc, char** argv) {
-  if (argc < 2)
-    return 0;
-    
+void serialize_deserialize(Data d) {
+  std::cout << "d.i       : " << std::dec << d.i << std::endl; 
+  std::cout << "d.c       : " << d.c << std::endl; 
+  std::cout << "d address : " << &d << std::endl; 
+  std::cout << std::endl; 
+
+  uintptr_t raw = serialize(&d);
+  Data* deserialized = deserialize(raw);
+
+  std::cout << "deserialized.i       : " << std::dec << deserialized->i << std::endl; 
+  std::cout << "deserialized.c       : " << deserialized->c << std::endl; 
+  std::cout << "deserialized address : " << deserialized << std::endl; 
+  std::cout << std::endl; 
+}
+
+int main(void) {
+  Data d;
+  d.i = 42;
+  d.c = 'a';
+  serialize_deserialize(d);
   return 0;
 }
