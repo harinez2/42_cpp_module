1e3b561ecae43cd2296b9d9d46bf79b16a402f4d
harinez2 <yonishi@student.42tokyo.jp>
2021-11-20 18:27:23 +0900

    ok for pointer identifier

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -10,7 +10,7 @@
 Base* generate(void) {
   if (std::rand() % 3 == 0) {
     std::cout << "generated A instance." << std::endl;
-    return new A();
+    return new A;
   }
   else if (std::rand() % 3 == 1) {
     std::cout << "generated B instance." << std::endl;
@@ -23,11 +23,29 @@ Base* generate(void) {
   return NULL;
 }
 
-void identify(const Base* p) {
-  (void)p;
+void identify(Base* p) {
+  A* a = dynamic_cast<A*>(p);
+  if (a) {
+    std::cout << "A" << std::endl;
+    return;
+  }
+
+  B* b = dynamic_cast<B*>(p);
+  if (b) {
+    std::cout << "B" << std::endl;
+    return;
+  }
+
+  C* c = dynamic_cast<C*>(p);
+  if (c) {
+    std::cout << "C" << std::endl;
+    return;
+  }
+  else
+    std::cout << "Cannot identify the opject type." << std::endl;
 }
 
-void identify(const Base& p) {
+void identify(Base& p) {
   (void)p;
 }
 
