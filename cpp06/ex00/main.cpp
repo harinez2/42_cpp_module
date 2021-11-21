ffc38789cfb32528587f4ad903c3be76adcd4cc3
harinez2 <yonishi@student.42tokyo.jp>
2021-11-21 21:05:17 +0900

    brush up main

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -2,37 +2,28 @@
 
 #include <iostream>
 
-void print_all_type(const char* s) {
+void print_all_type(const char* s, int dbgflg) {
+  if (dbgflg)
+    std::cout << "./convert " << s << std::endl;
   Convert c(s);
   c.printChar();
   c.printInt();
   c.printFloat();
   c.printDouble();
-}
-
-void print_basic_tests() {
-    std::cout << "Ex)" << std::endl;
-
-    const std::string s1 = "0";
-    std::cout << "./convert " << s1 << std::endl;
-    print_all_type(s1.c_str());
-
-    const std::string s2 = "nan";
-    std::cout << "./convert " << s2 << std::endl;
-    print_all_type(s2.c_str());
-    
-    const std::string s3 = "42.0f";
-    std::cout << "./convert " << s3 << std::endl;
-    print_all_type(s3.c_str());
+  if (dbgflg)
+    std::cout << std::endl;
 }
 
 int main(int argc, const char** argv) {
   if (argc != 2) {
     std::cout << "Usage: ./convert <converting value>" << std::endl;
     std::cout << std::endl;
-    print_basic_tests();
+    std::cout << "Ex)" << std::endl;
+    print_all_type("0", 1);
+    print_all_type("nan", 1);
+    print_all_type("42.0f", 1);
     return 0;
   }
-  print_all_type(argv[1]);
+  print_all_type(argv[1], 0);
   return 0;
 }
