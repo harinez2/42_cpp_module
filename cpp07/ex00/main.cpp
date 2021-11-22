02dec07682adadee6b07e06f5e5b48f40eb6d9a7
harinez2 <yonishi@student.42tokyo.jp>
2021-11-22 20:55:32 +0900

    compile error ver

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template <typename T>
+void ::swap(T& left, T& right);
+
+template <typename T>
+T& ::min(T& left, T& right);
+
+template <typename T>
+T& ::max(T& left, T& right);
+
+int main(void) {
+  int a = 2;
+  int b = 3;
+
+  ::swap( a, b );
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+  std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+  std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+  return 0;
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,23 @@
+
+template <typename T>
+void ::swap(T& left, T& right) {
+  T tmp = left;
+  left = right;
+  right = tmp;
+}
+
+template <typename T>
+T& ::min(T& left, T& right) {
+  if (left < right)
+    return left;
+  else
+    return right;
+}
+
+template <typename T>
+T& ::max(T& left, T& right) {
+  if (left > right)
+    return left;
+  else
+    return right;
+}
