d2672425b9b48f2a67ef4256d664dfb70693c79c
harinez2 <yonishi@student.42tokyo.jp>
2021-11-23 02:29:57 +0900

    ok

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,6 +1,10 @@
+#ifndef ITER_HPP
+#define ITER_HPP
 
-template <typename T>
-void iter(T* array_p, T& array_len, T* func) {
+template <typename T, typename U, typename F>
+void iter(T* array_p, U array_len, F func) {
   for (int i = 0; i < array_len; ++i)
     func(array_p[i]);
 }
+
+#endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,5 +1,36 @@
 #include "iter.hpp"
 
+#include <iostream>
+#include <string>
+
+template <typename T>
+void twicer(T& arg) {
+  arg = arg + arg;
+}
+
+template <typename T>
+void print_it(T& elem) {
+  std::cout << elem << " ";
+}
+
 int main(void) {
+  int array_int[] = {4, 2, 42};
+  const int array_int_size = sizeof(array_int) / sizeof (int);
+
+  iter(array_int, array_int_size, &print_it<int>);
+  std::cout << std::endl;
+  iter(array_int, array_int_size, &twicer<int>);
+  iter(array_int, array_int_size, &print_it<int>);
+  std::cout << std::endl;
+
+
+  std::string array_str[] = {"four", "two", "fourty-two"};
+  int array_str_size = sizeof(array_str) / sizeof (std::string);
+
+  iter(array_str, array_str_size, &print_it<std::string>);
+  std::cout << std::endl;
+  iter(array_str, array_str_size, &twicer<std::string>);
+  iter(array_str, array_str_size, &print_it<std::string>);
+  std::cout << std::endl;
   return 0;
 }
