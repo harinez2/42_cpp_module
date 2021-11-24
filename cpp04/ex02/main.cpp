014fb9a7860f3a243e4cc706154a1a0b3bfef1ed
harinez2 <yonishi@student.42tokyo.jp>
2021-11-25 01:26:41 +0900

    added endl in main

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -7,9 +7,12 @@
 void test_simple() {
   std::cout << "-----test_simple" << std::endl;
   const Animal* j = new Dog();
+  std::cout << std::endl;
   const Animal* i = new Cat();
+  std::cout << std::endl;
 
   delete j;//should not create a leak
+  std::cout << std::endl;
   delete i;
   std::cout << std::endl;
 }
