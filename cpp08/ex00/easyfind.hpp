875d25db6c8a3f473b3a7874314dd2399accea58
harinez2 <yonishi@student.42tokyo.jp>
2021-11-26 00:28:54 +0900

    supported string

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include <algorithm>
 
-template <typename T>
-typename T::iterator easyFind(T& arr, int search_int) {
+template <typename T, typename U>
+typename T::iterator easyFind(T& arr, U search_int) {
   return std::find(arr.begin(), arr.end(), search_int);
 }
 
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -10,10 +10,10 @@ void print_vec(T& vec) {
   std::cout << std::endl;
 }
 
-template <typename T>
-void print_easyfind_result(T& vec, int n) {
+template <typename T, typename U>
+void print_easyfind_result(T& vec, U n) {
   std::cout << "searching " << n << " ... ";
-  typename T::iterator it = easyFind<T>(vec, n);
+  typename T::iterator it = easyFind<T, U>(vec, n);
   if (it != vec.end())
     std::cout << "found: " << *it << std::endl;
   else
@@ -28,14 +28,14 @@ int main(void) {
   print_easyfind_result(vec_int, 3);
   print_easyfind_result(vec_int, 2);
   print_easyfind_result(vec_int, 9);
+  std::cout << std::endl;
 
-  
-  // std::string str[] = {"a", "bb", "ccc", "dddd"};
-  // std::vector<std::string> vec_str(str, str + sizeof(str) / sizeof(std::string));
-  // print_vec(vec_str);
-  // print_easyfind_result(vec_str, "a");
-  // print_easyfind_result(vec_str, "ccc");
-  // print_easyfind_result(vec_str, "dddd");
-  // print_easyfind_result(vec_str, "d");
+  std::string str[] = {"a", "bb", "ccc", "dddd"};
+  std::vector<std::string> vec_str(str, str + sizeof(str) / sizeof(std::string));
+  print_vec(vec_str);
+  print_easyfind_result(vec_str, "a");
+  print_easyfind_result(vec_str, "ccc");
+  print_easyfind_result(vec_str, "dddd");
+  print_easyfind_result(vec_str, "d");
   return 0;
 }
