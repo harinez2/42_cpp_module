1b6a1036bc288d3c375d13be10a5d8bafd2e8d98
harinez2 <yonishi@student.42tokyo.jp>
2021-11-26 01:28:25 +0900

    added deque and set test case && removed string param support

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -4,13 +4,8 @@
 #include <vector>
 #include <algorithm>
 
-template <typename T, typename U>
-typename T::iterator easyFind(T& arr, U search_int) {
-  return std::find(arr.begin(), arr.end(), search_int);
-}
-
-template <typename T, typename U>
-typename T::const_iterator easyFind(const T& arr, U search_int) {
+template <typename T>
+typename T::const_iterator easyFind(const T& arr, int search_int) {
   return std::find(arr.begin(), arr.end(), search_int);
 }
 
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -3,13 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <list>
-
-template <typename T>
-void print_vec(T& vec) {
-  for (typename T::iterator it = vec.begin(); it != vec.end(); ++it)
-    std::cout << *it << " ";
-  std::cout << std::endl;
-}
+#include <deque>
+#include <set>
 
 template <typename T>
 void print_vec(const T& vec) {
@@ -18,20 +13,10 @@ void print_vec(const T& vec) {
   std::cout << std::endl;
 }
 
-template <typename T, typename U>
-void print_easyfind_result(T& vec, U n) {
+template <typename T>
+void print_easyfind_result(const T& vec, int n) {
   std::cout << "searching " << n << " ... ";
-  typename T::iterator it = easyFind<T, U>(vec, n);
-  if (it != vec.end())
-    std::cout << "found: " << *it << std::endl;
-  else
-    std::cout << "not found." << std::endl;
-}
-
-template <typename T, typename U>
-void print_easyfind_result(const T& vec, U n) {
-  std::cout << "searching " << n << " ... ";
-  typename T::const_iterator it = easyFind<const T, U>(vec, n);
+  typename T::const_iterator it = easyFind<const T>(vec, n);
   if (it != vec.end())
     std::cout << "found: " << *it << std::endl;
   else
@@ -80,21 +65,52 @@ void test_list() {
   std::cout << std::endl;
 }
 
-void test_vector_string() {
-  std::cout << "std::vector<std::string>" << std::endl;
-  std::string str[] = {"a", "bb", "ccc", "dddd"};
-  std::vector<std::string> vec_str(str, str + sizeof(str) / sizeof(std::string));
-  print_vec(vec_str);
-  print_easyfind_result(vec_str, "a");
-  print_easyfind_result(vec_str, "ccc");
-  print_easyfind_result(vec_str, "dddd");
-  print_easyfind_result(vec_str, "d");
+void test_deque() {
+  int intarr[] = {1, 4, 3, 5, 2};
+  std::cout << "std::deque<int>" << std::endl;
+  std::deque<int> deque_int(intarr, intarr + sizeof(intarr) / sizeof(int));
+  print_vec(deque_int);
+  print_easyfind_result(deque_int, 1);
+  print_easyfind_result(deque_int, 3);
+  print_easyfind_result(deque_int, 2);
+  print_easyfind_result(deque_int, 9);
+  std::cout << std::endl;
+
+  std::cout << "const std::deque<int>" << std::endl;
+  const std::deque<int> deque_c_int(intarr, intarr + sizeof(intarr) / sizeof(int));
+  print_vec(deque_c_int);
+  print_easyfind_result(deque_c_int, 1);
+  print_easyfind_result(deque_c_int, 3);
+  print_easyfind_result(deque_c_int, 2);
+  print_easyfind_result(deque_c_int, 9);
+  std::cout << std::endl;
+}
+
+void test_set() {
+  int intarr[] = {1, 4, 3, 5, 2};
+  std::cout << "std::set<int>" << std::endl;
+  std::set<int> set_int(intarr, intarr + sizeof(intarr) / sizeof(int));
+  print_vec(set_int);
+  print_easyfind_result(set_int, 1);
+  print_easyfind_result(set_int, 3);
+  print_easyfind_result(set_int, 2);
+  print_easyfind_result(set_int, 9);
+  std::cout << std::endl;
+
+  std::cout << "const std::set<int>" << std::endl;
+  const std::set<int> set_c_int(intarr, intarr + sizeof(intarr) / sizeof(int));
+  print_vec(set_c_int);
+  print_easyfind_result(set_c_int, 1);
+  print_easyfind_result(set_c_int, 3);
+  print_easyfind_result(set_c_int, 2);
+  print_easyfind_result(set_c_int, 9);
   std::cout << std::endl;
 }
 
 int main(void) {
   test_vector();
   test_list();
-  test_vector_string();
+  test_deque();
+  test_set();
   return 0;
 }
