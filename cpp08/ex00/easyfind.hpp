80384366a1e1bd9151944225cbffb01dccdc2f4a
harinez2 <yonishi@student.42tokyo.jp>
2021-11-26 01:11:24 +0900

    supported const int

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -9,4 +9,9 @@ typename T::iterator easyFind(T& arr, U search_int) {
   return std::find(arr.begin(), arr.end(), search_int);
 }
 
+template <typename T, typename U>
+typename T::const_iterator easyFind(const T& arr, U search_int) {
+  return std::find(arr.begin(), arr.end(), search_int);
+}
+
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -10,6 +10,13 @@ void print_vec(T& vec) {
   std::cout << std::endl;
 }
 
+template <typename T>
+void print_vec(const T& vec) {
+  for (typename T::const_iterator it = vec.begin(); it != vec.end(); ++it)
+    std::cout << *it << " ";
+  std::cout << std::endl;
+}
+
 template <typename T, typename U>
 void print_easyfind_result(T& vec, U n) {
   std::cout << "searching " << n << " ... ";
@@ -20,7 +27,18 @@ void print_easyfind_result(T& vec, U n) {
     std::cout << "not found." << std::endl;
 }
 
+template <typename T, typename U>
+void print_easyfind_result(const T& vec, U n) {
+  std::cout << "searching " << n << " ... ";
+  typename T::const_iterator it = easyFind<const T, U>(vec, n);
+  if (it != vec.end())
+    std::cout << "found: " << *it << std::endl;
+  else
+    std::cout << "not found." << std::endl;
+}
+
 int main(void) {
+  std::cout << "std::vector<int>" << std::endl;
   int intarr[] = {1, 4, 3, 5, 2};
   std::vector<int> vec_int(intarr, intarr + sizeof(intarr) / sizeof(int));
   print_vec(vec_int);
@@ -30,6 +48,7 @@ int main(void) {
   print_easyfind_result(vec_int, 9);
   std::cout << std::endl;
 
+  std::cout << "std::vector<std::string>" << std::endl;
   std::string str[] = {"a", "bb", "ccc", "dddd"};
   std::vector<std::string> vec_str(str, str + sizeof(str) / sizeof(std::string));
   print_vec(vec_str);
@@ -37,5 +56,17 @@ int main(void) {
   print_easyfind_result(vec_str, "ccc");
   print_easyfind_result(vec_str, "dddd");
   print_easyfind_result(vec_str, "d");
+  std::cout << std::endl;
+  
+  std::cout << "const std::vector<int>" << std::endl;
+  int c_intarr[] = {1, 4, 3, 5, 2};
+  const std::vector<int> vec_c_int(c_intarr, c_intarr + sizeof(c_intarr) / sizeof(int));
+  print_vec(vec_c_int);
+  print_easyfind_result(vec_c_int, 1);
+  print_easyfind_result(vec_c_int, 3);
+  print_easyfind_result(vec_c_int, 2);
+  print_easyfind_result(vec_c_int, 9);
+  std::cout << std::endl;
+
   return 0;
 }
