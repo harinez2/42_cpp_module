cd5cd17c9d3d7f9cdee7b7ce59dccda96ece08bb
harinez2 <yonishi@student.42tokyo.jp>
2021-11-26 23:05:17 +0900

    ok

--- cpp08/ex02/mutantstack.hpp
+++ cpp08/ex02/mutantstack.hpp
@@ -1,13 +1,24 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#include <stack>
+
 template <typename T>
-class MutantStack {
+class MutantStack : public std::stack<T> {
  public:
-  void push(int n);
-  int pop();
-  int top();
-  int size();
+  typedef typename std::stack<T>::container_type::iterator iterator;
+
+  MutantStack() {}
+  MutantStack(const MutantStack& rhs) { *this = rhs; }
+  MutantStack& operator=(const MutantStack& rhs) {
+    if (this != &rhs) {
+    }
+    return *this;
+  }
+  ~MutantStack() {}
+
+  iterator begin() { return this->c.begin(); }
+  iterator end() { return this->c.end(); }
 };
 
 #endif
