b6f5073568a60dff943a631ab8827b0693e6d2c4
harinez2 <yonishi@student.42tokyo.jp>
2021-11-26 15:52:28 +0900

    added tests

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,6 +1,30 @@
 #include "span.hpp"
 
 #include <iostream>
+#include <climits>
+
+void test_setsize() {
+  Span sp = Span(0);
+  Span sp2 = Span(UINT_MAX);
+}
+
+void test_add() {
+  Span sp = Span(3);
+  sp.addNumber(1);
+  sp.addNumber(2);
+  try {
+    sp.addNumber(2);
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+  sp.addNumber(3);
+  try {
+    sp.addNumber(4);
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+  std::cout << std::endl;
+}
 
 void test_subject() {
   Span sp = Span(5);
@@ -16,7 +40,8 @@ void test_subject() {
 }
 
 int main(void) {
-
+  test_setsize();
+  test_add();
   test_subject();
   return 0;
 }
